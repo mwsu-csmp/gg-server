aae333748469864ec27e9b3b728c2d9523160550
Celery1969 <spoirier3@missouriwestern.edu>
2019-05-28 21:19:15 -0500

    added the logger in PlayerController

--- src/main/java/hello/PlayerController.java
+++ src/main/java/hello/PlayerController.java
@@ -4,14 +4,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Controller
 public class PlayerController {
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @MessageMapping(value = "/hello")
     @SendTo("/topic/moveto")
     public Mapping map(Player player) throws Exception{
+        log.info("getting the player position");
         return new Mapping(HtmlUtils.htmlEscape(Integer.toString(player.getX())));
     }
 
