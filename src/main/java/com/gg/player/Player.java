4b8ac7cf5565f912ac4c9138bb3f15512c64460f
McKFlat <38057035+McKFlat@users.noreply.github.com>
2019-06-17 15:55:06 -0500

    added methods for Y coordinate

--- src/main/java/com/gg/player/Player.java
+++ src/main/java/com/gg/player/Player.java
@@ -11,19 +11,32 @@ public class Player  {
     public Player(int x){
         this.x = x;
     }
-
     public Player(String x){
         this.x = Integer.parseInt(x);
+    }
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Player(String x, String y){
+        this.x = Integer.parseInt(x);
+        this.y = Integer.parseInt(y);
     }
 
     public int updateX(int x){
         this.x = x;
         return this.x;
     }
-
+    public int updateY(int y){
+        this.y = y;
+        return this.y;
+    }
 
     public int getX(){
         return x;
+    }
+    public int getY(){
+        return y;
     }
 
 
