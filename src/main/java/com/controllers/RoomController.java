55a85dea59ceda800f681c4d28c97f2b076719a9
jmorwick <jbmorwick@gmail.com>
2019-07-13 16:52:14 -0500

    added controller to serialize boards to json #5

--- src/main/java/com/controllers/RoomController.java
+++ src/main/java/com/controllers/RoomController.java
@@ -1,9 +1,15 @@
 package com.controllers;
 
 import com.rooms.Sandbox;
+import edu.missouriwestern.csmp.gg.base.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -12,6 +18,18 @@ import org.slf4j.Logger;
 public class RoomController {
 
     Logger log = LoggerFactory.getLogger(this.getClass());
+
+
+    @Autowired
+    private Game game;
+
+    @GetMapping("/board/{boardId}")
+    @ResponseBody
+    public String getBoard(@PathVariable String boardId) {
+        log.info("reading " + boardId);
+        var board = game.getBoard(boardId);
+        return board.toString();
+    }
 
     @MessageMapping(value = "/com/board")
     @SendTo("/topic/room")
